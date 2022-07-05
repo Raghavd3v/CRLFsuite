fe2a096ea8d51eb32cd7b2594eaf8f7d2ade2b3b
Nefcore Security <99230389+Nefcore@users.noreply.github.com>
2022-07-05 21:01:28 +0530

    Update __main__.py

--- crlfsuite/__main__.py
+++ crlfsuite/__main__.py
@@ -16,7 +16,7 @@ dt_string = now.strftime("%d-%m-%Y %H:%M")
 
 if not silent:
     args_info(url, urls, std, output_file, method, data, cookies, timeout, user_agent["User-Agent"], verify, threads)
-    logger.info('CRLFsuite v2.0.1 (%sCRLF vulnerability Scanner%s)' % (green,reset))
+    logger.info('CRLFsuite v2.1.1 (%sCRLF vulnerability Scanner%s)' % (green,reset))
     logger.warn('Use with caution. You are responsible for your actions')
     logger.warn('Developers assume no liability and are not responsible for any misuse or damage.')
 
